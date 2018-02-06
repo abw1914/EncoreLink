5bcc3800aef5ba0961115fa4127dbdd69866f03a
aaron <aaronbwillis@msn.com>
2018-02-06 15:38:45 -0700

    Added new classes and started on login xml

---
+++ app/src/main/java/com/example/awillis/encorelink/MusicianAuthentication.java
@@ -0,0 +1,12 @@
+package com.example.awillis.encorelink;
+
+/**
+ * Created by awillis on 2/6/18.
+ */
+
+public class MusicianAuthentication {
+}
+
+
+//TODO Add facebook authentication class
+ //TODO add firebase to Manifest file
