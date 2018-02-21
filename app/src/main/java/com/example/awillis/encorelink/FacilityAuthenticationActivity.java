af16c24af4d533a574d7e0ee62bfc49ac7ed7a30
Baghaii <baghaii@gmail.com>
2018-02-20 20:01:59 -0700

    Add xml files for authentication activities
    Display placeholder text for each authentication screen.

--- app/src/main/java/com/example/awillis/encorelink/FacilityAuthenticationActivity.java
+++ app/src/main/java/com/example/awillis/encorelink/FacilityAuthenticationActivity.java
@@ -1,5 +1,7 @@
 package com.example.awillis.encorelink;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -8,5 +10,9 @@ import android.support.v7.app.AppCompatActivity;
 
 public class FacilityAuthenticationActivity extends AppCompatActivity {
 
-
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_facility_authentication);
+  }
 }
--- app/src/main/java/com/example/awillis/encorelink/MusicianAuthenticationActivity.java
+++ app/src/main/java/com/example/awillis/encorelink/MusicianAuthenticationActivity.java
@@ -1,5 +1,7 @@
 package com.example.awillis.encorelink;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,7 +9,11 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class MusicianAuthenticationActivity extends AppCompatActivity {
-
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_musician_authentication);
+  }
 }
 
 
