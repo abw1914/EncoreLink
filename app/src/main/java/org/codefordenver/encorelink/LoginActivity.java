2543522f7af69e0e0295f6f18c48577528937a4c
jqn <jqn@Macbooks-MacBook-Pro.local>
2018-04-12 07:35:21 -0600

    Added password validation

--- app/src/main/java/org/codefordenver/encorelink/LoginActivity.java
+++ app/src/main/java/org/codefordenver/encorelink/LoginActivity.java
@@ -16,7 +16,7 @@ public class LoginActivity extends AppCompatActivity {
     private static final int REQUEST_SIGNUP = 0;
 
     private static EditText emailText;
-    private static EditText password;
+    private static EditText passwordText;
     private static Button login_btn;
 
     @Override
@@ -28,7 +28,7 @@ public class LoginActivity extends AppCompatActivity {
 
     public void Login() {
         emailText = findViewById(R.id.input_email);
-        password = findViewById(R.id.input_password);
+        passwordText = findViewById(R.id.input_password);
         login_btn = findViewById(R.id.button_login);
 
         login_btn.setOnClickListener(
@@ -59,13 +59,20 @@ public class LoginActivity extends AppCompatActivity {
     public boolean validate() {
         boolean valid = true;
         String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
 
-
-        if(email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+        if  (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             emailText.setError("Enter a valid email address");
             valid = false;
         } else {
             emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            passwordText.setError("use a password between 4 to 10 characters");
+            valid = false;
+        } else {
+            passwordText.setError(null);
         }
 
         return valid;
