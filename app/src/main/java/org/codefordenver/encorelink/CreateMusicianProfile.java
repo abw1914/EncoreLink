f5c8159b016d8532be271f47d51facfbf34b734e
Ben Morris-Rains <benmorrisrains@gmail.com>
2018-05-03 15:34:24 -0600

    Added musician profile creation activity.

---
+++ app/src/main/java/org/codefordenver/encorelink/CreateMusicianProfile.java
@@ -0,0 +1,102 @@
+package org.codefordenver.encorelink;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class CreateMusicianProfile extends AppCompatActivity {
+
+    public static final String MUSICIAN_PROFILE = "musician_profile";
+    private EditText firstName;
+    private EditText lastName;
+    private EditText phoneNumber;
+    private EditText streetAddress;
+    private EditText city;
+    private EditText zipcode;
+    private EditText musicalTalent;
+    private EditText videoLink;
+    private Button saveButton;
+
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+    private MusicianEntity musicianEntity = new MusicianEntity();
+    public static String userId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_musician_profile);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            userId = user.getUid();
+        }
+
+        firstName = findViewById(R.id.musician_first_name);
+        lastName = findViewById(R.id.musician_last_name);
+        phoneNumber = findViewById(R.id.musician_phonenumber);
+        streetAddress = findViewById(R.id.musician_city);
+        city = findViewById(R.id.musician_city);
+        zipcode = findViewById(R.id.musician_zipcode);
+        musicalTalent = findViewById(R.id.musician_talent);
+        videoLink = findViewById(R.id.video_link);
+        saveButton = findViewById(R.id.save_musician_profile);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                musicianEntity.setFirstName(firstName.getText().toString());
+                musicianEntity.setLastName(lastName.getText().toString());
+                musicianEntity.setPhoneNumber(phoneNumber.getText().toString());
+                musicianEntity.setStreetAddress(streetAddress.getText().toString());
+                musicianEntity.setCity(city.getText().toString());
+                musicianEntity.setZipcode(zipcode.getText().toString());
+                musicianEntity.setMusicalTalent(musicalTalent.getText().toString());
+                musicianEntity.setVideoLink(videoLink.getText().toString());
+
+                if (musicianEntity.getFirstName().isEmpty()) {
+                    Toast.makeText(CreateMusicianProfile.this, "First name is required!", Toast.LENGTH_SHORT).show();
+                }
+                if (musicianEntity.getLastName().isEmpty()) {
+                    Toast.makeText(CreateMusicianProfile.this, "Last name is required!", Toast.LENGTH_SHORT).show();
+                }
+                if (musicianEntity.getPhoneNumber().isEmpty()) {
+                    Toast.makeText(CreateMusicianProfile.this, "Phone number is required!", Toast.LENGTH_SHORT).show();
+                } else {
+
+
+                    databaseReference.child(MUSICIAN_PROFILE).child(userId).setValue(musicianEntity);
+
+                    Toast.makeText(CreateMusicianProfile.this, "Saving profile information.", Toast.LENGTH_SHORT).show();
+//                    Intent musicianDashboard = new Intent(CreateMusicianProfile.this, null);
+
+
+
+                }
+
+
+
+
+
+
+
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/org/codefordenver/encorelink/MusicianEntity.java
@@ -0,0 +1,91 @@
+package org.codefordenver.encorelink;
+
+import android.widget.EditText;
+
+public class MusicianEntity {
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String streetAddress;
+    private String city;
+    private String zipcode;
+    private String musicalTalent;
+    private String videoLink;
+    private String musicianId;
+
+    public String getMusicianId() {
+        return musicianId;
+    }
+
+    public void setMusicianId(String musicianId) {
+
+        musicianId = CreateMusicianProfile.userId;
+
+        this.musicianId = musicianId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getMusicalTalent() {
+        return musicalTalent;
+    }
+
+    public void setMusicalTalent(String musicalTalent) {
+        this.musicalTalent = musicalTalent;
+    }
+
+    public String getVideoLink() {
+        return videoLink;
+    }
+
+    public void setVideoLink(String videoLink) {
+        this.videoLink = videoLink;
+    }
+}
