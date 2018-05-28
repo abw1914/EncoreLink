77864d44e88b6325a1b08d8eb0ac195b1fbaa123
Ben Morris-Rains <benmorrisrains@gmail.com>
2018-05-28 17:28:50 -0600

    WIP

--- app/src/main/java/org/codefordenver/encorelink/OrganizerCreateEvent.java
+++ app/src/main/java/org/codefordenver/encorelink/OrganizerCreateEvent.java
@@ -1,13 +1,29 @@
 package org.codefordenver.encorelink;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class OrganizerCreateEvent extends AppCompatActivity {
+
+    private EditText eventTitle;
+    private EditText streetAddress;
+    private EditText city;
+    private EditText zipcode;
+    private EditText startTime;
+    private EditText endTime;
+    private TextInputEditText notes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organizer_create_event);
+
+        eventTitle = findViewById(R.id.addEventTitleEditText);
+        streetAddress = findViewById(R.id.addEventAddressEditText);
+        city = findViewById(R.id.addEventCityEditText)
+
     }
 }
